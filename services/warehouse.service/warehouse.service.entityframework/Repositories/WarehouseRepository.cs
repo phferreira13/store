28c1916803fb0af134611c52f58886a019b545bc
Paulo Henrique Ferreira <paulo.hf.ferreira13@gmail.com>
2024-11-21 22:05:16 -0300

    Refatoração de namespaces e ciclo de vida de serviços
    Alterações detalhadas:
    
    * No arquivo `RepositoriesStartup.cs`, as referências de namespace foram alteradas de `warehouse.service.domain.Repositories` para `warehouse.service.entityframewor.Repositories`. Além disso, os serviços `IWarehouseRepository` e `IItemRepository` foram alterados de `AddSingleton` para `AddScoped`.
    
    * No arquivo `ItemRepository.cs`, o namespace foi alterado de `warehouse.service.domain.Repositories` para `warehouse.service.entityframewor.Repositories`. Todo o código da classe `ItemRepository` foi removido e reescrito sob o novo namespace, mas a implementação da classe permaneceu a mesma.
    
    * No arquivo `WarehouseRepository.cs`, o namespace foi alterado de `warehouse.service.domain.Repositories` para `warehouse.service.entityframewor.Repositories`. Todo o código da classe `WarehouseRepository` foi removido e reescrito sob o novo namespace, mas a implementação da classe permaneceu a mesma.

---
+++ services/warehouse.service/warehouse.service.entityframework/Repositories/WarehouseRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using warehouse.service.domain.Interfaces.Repositories;
+using warehouse.service.domain.Models;
+
+namespace warehouse.service.entityframewor.Repositories;
+
+public class WarehouseRepository : IWarehouseRepository
+{
+    private List<Warehouse> _warehouses = [];
+
+    public WarehouseRepository()
+    { }
+
+    public Warehouse? GetWarehouse(Guid id)
+    {
+        return _warehouses.FirstOrDefault(x => x.Id == id);
+    }
+
+    public IEnumerable<Warehouse> GetWarehouses()
+    {
+        return _warehouses;
+    }
+
+    public void AddWarehouse(Warehouse warehouse)
+    {
+        _warehouses.Add(warehouse);
+    }
+
+    public void UpdateWarehouse(Guid id, string name, string location)
+    {
+        var warehouse = GetWarehouse(id);
+        warehouse?.Update(name, location);
+    }
+
+    public void AddItem(Guid warehouseId, Item item, int quantity)
+    {
+        var warehouse = GetWarehouse(warehouseId);
+        warehouse?.AddItem(item, quantity);
+    }
+
+    public void IncreaseItemQuantity(Guid warehouseId, Guid itemId, int quantity = 1)
+    {
+        var warehouse = GetWarehouse(warehouseId);
+        warehouse?.IncreaseItemQuantity(itemId, quantity);
+    }
+
+    public void DecreaseItemQuantity(Guid warehouseId, Guid itemId, int quantity = 1)
+    {
+        var warehouse = GetWarehouse(warehouseId);
+        warehouse?.DecreaseItemQuantity(itemId, quantity);
+    }
+
+    public void DeleteWarehouse(Guid id)
+    {
+        var warehouse = GetWarehouse(id);
+        if (warehouse != null)
+            _warehouses.Remove(warehouse);
+    }
+}
